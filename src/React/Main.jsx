a7b2bb3cc5d1a3e4eb4254070bf7511cbb88fad7
Ran <rzhang@ZR-MACBOOKPRO.attlocal.net>
2021-01-11 07:42:37 -0800

    My first commint

---
+++ src/React/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* Components ---------------------------*/
+// import Test from './Test.jsx';
+
+const Main = () => {
+
+    return (
+        <div className='Main'>
+            <h1>Ran's In Class</h1>
+        </div>
+    );
+}
+
+export default Main;
